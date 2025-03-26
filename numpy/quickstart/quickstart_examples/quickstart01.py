b14cb029c03a83e0d451aff6df8189730f41c2f7
allenkang92 <ksw8841@knou.ac.kr>
2025-03-26 10:43:03 +0900

    upload : python files and md file.

---
+++ numpy/quickstart/quickstart_examples/quickstart01.py
@@ -0,0 +1,33 @@
+import numpy as np
+a = np.arange(15).reshape(3, 5)
+
+print(a)
+# [[ 0  1  2  3  4]
+#  [ 5  6  7  8  9]
+#  [10 11 12 13 14]]
+
+print(a.shape)
+# (3, 5)
+
+print(a.ndim)
+# 2
+
+print(a.dtype.name)
+# int64
+
+print(a.itemsize)
+# 8
+
+print(a.size)
+# 15
+
+print(type(a))
+# <class 'numpy.ndarray'>
+
+b = np.array([6, 7, 8])
+
+print(b)
+# [6 7 8]
+
+print(type(b))
+# <class 'numpy.ndarray'>
---
+++ numpy/quickstart/quickstart_examples/quickstart02.py
@@ -0,0 +1,15 @@
+
+
+import numpy as np
+a = np.array([2, 3, 4])
+
+print(a)
+# [2 3 4]
+
+print(a.dtype)
+# int64
+
+b = np.array([1.2, 3.5, 5.1])
+
+print(b.dtype)
+# float64
